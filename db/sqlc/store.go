21fd9f553671c2e27242983fb4e9554cb2537992
Nozomu Hatta <nozomuhatta@gmail.com>
2021-10-25 22:26:21 +0900

    Add GetAccountForUpdate
    At this point, TestTransferTx fails.

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -87,7 +87,7 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 
-		account1, err := q.GetAccount(ctx, arg.FromAccountID)
+		account1, err := q.GetAccountForUpdate(ctx, arg.FromAccountID)
 		if err != nil {
 			return err
 		}
@@ -100,7 +100,7 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 
-		account2, err := q.GetAccount(ctx, arg.ToAccountID)
+		account2, err := q.GetAccountForUpdate(ctx, arg.ToAccountID)
 		if err != nil {
 			return err
 		}
