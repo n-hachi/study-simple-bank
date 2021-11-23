1887b4ef97cc2a3029ffece84671ff6b4a526469
Nozomu Hatta <nozomuhatta@gmail.com>
2021-11-23 23:52:13 +0900

    Add app.env and change to use config file

--- main.go
+++ main.go
@@ -7,16 +7,12 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/n-hachi/study-simple-bank/api"
 	db "github.com/n-hachi/study-simple-bank/db/sqlc"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
+	"github.com/n-hachi/study-simple-bank/util"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -24,7 +20,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
