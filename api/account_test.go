0825ce4c8f5652fdf51017d9e5d88985721d00fe
Nozomu Hatta <nozomuhatta@gmail.com>
2021-12-18 02:48:34 +0900

    Update TestListAccountAPI for internal server error

--- api/account_test.go
+++ api/account_test.go
@@ -241,6 +241,22 @@ func TestListAccountAPI(t *testing.T) {
 				require.Equal(t, http.StatusOK, recorder.Code)
 			},
 		},
+		{
+			name: "InternalError",
+			query: Query{
+				pageID:   1,
+				pageSize: n,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					ListAccounts(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return([]db.Account{}, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
 	}
 
 	for i := range testCases {
